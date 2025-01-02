fbe5714f9e472fc28ded267a8bf72f112fa9f869
mahender <gmahe139@gmail.com>
2025-01-02 19:49:52 +0530

    new code added

--- 04-data-type.sh
+++ 04-data-type.sh
@@ -6,5 +6,5 @@ NUMBER3=$3
 TIMESTAMP=$(date)
 
 echo "Timestamp of the script executed: $TIMESTAMP"
-SUM=$($NUMBER1+$NUMBER2+$NUMBER3)
+SUM=$(($NUMBER1+$NUMBER2+$NUMBER3))
 echo "Sum of $NUMBER1 and $NUMBER2 and $NUMBER3 is $SUM"
