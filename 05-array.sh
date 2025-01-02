d015377134e8645e206ba0bf4eaaee566e63d017
mahender <gmahe139@gmail.com>
2025-01-02 20:14:54 +0530

    new code added

--- 05-array.sh
+++ 05-array.sh
@@ -4,3 +4,5 @@ MOVIES=("amaran" "rrr" "bahubali")
 echo "First movie in the list is ${MOVIES[0]}"
 echo "Second movie in the list is ${MOVIES[1]}"
 echo "Third movie in the list is ${MOVIES[2]}"
+
+echo "All Movies in the list are ${MOVIES[@]}
