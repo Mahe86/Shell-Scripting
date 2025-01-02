a6be1b69d88e6b64716a84a43de09aa21f3191b7
mahender <gmahe139@gmail.com>
2025-01-02 19:13:10 +0530

    new code added

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,5 +2,5 @@ echo "Please enter your username"
 read USERNAME
 echo "username entered: $USERNAME"
 echo "Please enter your password"
-read -s PASSWORD
-echo "password entered: $PASSWORD"
+read -s PASSWORD # -s will not display the value entered.
+echo "password entered: $PASSWORD" # we should not disply the password. This is just for testing
