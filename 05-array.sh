ae9d445f0618a15c62af15db57dff78a4d8f1d0d
mahender <gmahe139@gmail.com>
2025-01-02 20:12:03 +0530

    new code added

--- 05-array.sh
+++ 05-array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 MOVIES=("amaran" "rrr" "bahubali")
-echo "First movie in the list is $MOVIES([0])"
-echo "Second movie in the list is $MOVIES([1])"
-echo "Third movie in the list is $MOVIES([2])"
+echo "First movie in the list is ${MOVIES[0]}"
+echo "Second movie in the list is $MOVIES[1]"
+echo "Third movie in the list is ${MOVIES[2]}"
