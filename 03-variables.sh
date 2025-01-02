203019dac5bf99851038472296b5ba90fd16ecb4
mahender <gmahe139@gmail.com>
2025-01-02 19:09:35 +0530

    new code added

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,3 +1,6 @@
 echo "Please enter your username"
 read USERNAME
-echo "username enetered: $USERNAME"
+echo "username entered: $USERNAME"
+echo "Please enter your password"
+read -s PASSWORD
+echo "password entered: $PASSWORD"
