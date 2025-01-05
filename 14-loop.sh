45b4544d77bafaf19fdbb5b43e97b3a935e241dc
mahender <gmahe139@gmail.com>
2025-01-05 20:41:31 +0530

    code updated

---
+++ 14-loop.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u) # Display UserID value, For root user its 0
+R="\e[31m" # Print Red color
+G="\e[32m" # Print Green color
+Y="\e[33m" # Print Yellow color
+N="\e[0m" # Print Default White color
+LOGS_FOLDER="/var/log/shellscriptlogs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+mkdir -p $LOGS_FOLDER
+echo "Created the $LOGS_FOLDER directory"
+echo "Started the shell script at $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR : User does not have SUDO access to execute this script"
+    exit 1 # Other than 0
+fi
+
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2..$G SUCCESS $N"
+    fi
+}
+
+for package in $@
+do 
+    dnf list installed $package &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    dnf install $package -y &>>$LOG_FILE_NAME
+    validate $? "Installing $package"
+else
+    echo -e "$package is..$Y already installed $N"
+fi
+done
