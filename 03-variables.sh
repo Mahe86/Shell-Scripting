081ce24f82cf9398f209e647e8cfe9b96e452ee1
mahender <gmahe139@gmail.com>
2025-01-07 09:07:14 +0530

    code updated

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Please enter your username"
-read USERNAME # read command will read the value of the entered command.
+read USERNAME # read command will read the entered value
 echo "username entered: $USERNAME"
 echo "Please enter your password"
 read -s PASSWORD # -s will not display the value entered.
